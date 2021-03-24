438e768d1f9fba9eb3a0aaf34d8132666ec71a80
KnightShade07 <45324806+KnightShade07@users.noreply.github.com>
2021-03-23 22:26:26 -0700

    Capitalized E on line 109

--- FinalExam.cpp
+++ FinalExam.cpp
@@ -106,7 +106,7 @@ void displayDeck(array<const string*, DECK_SIZE> deck) {
 
 */
 void shuffleDeck(array<const string*, DECK_SIZE>* deck) {
-	cout << "The deck has been shuffled! To see your new shuffled deck, press 2 and then enter!:" << endl;
+	cout << "The deck has been shuffled! To see your new shuffled deck, press 2 and then Enter!:" << endl;
 	static default_random_engine randEngine (static_cast<unsigned int>(time(0)));
 	for (unsigned int i = DECK_SIZE - 1; i > 0; i--)
 	{
